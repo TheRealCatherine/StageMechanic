bfa85609882cc57ece6880c9eb175cf3069efbac
CrispyHanako <crispyhanako@gmail.com>
2017-09-23 01:00:48 -0400

    Added follow Camera

---
+++ StageMechanic/Assets/Blocks/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject Cursor;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Cursor.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Cursor.transform.position + offset;
+    }
+}
