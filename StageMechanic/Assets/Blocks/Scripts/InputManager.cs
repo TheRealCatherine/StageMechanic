05b81a3fcafce965aef538b820c146f68fcfe7c1
Catherine <catherineirkalla@gmail.com>
2017-10-01 17:47:29 -0500

    Improve XBox360 input mapping

--- StageMechanic/Assets/Blocks/Scripts/InputManager.cs
+++ StageMechanic/Assets/Blocks/Scripts/InputManager.cs
@@ -40,6 +40,9 @@ public class InputManager : MonoBehaviour {
 	float rotationY = 0.0f;
 	float rotationX = 0.0f;
 
+	float period = 0.0f;
+	const float joystickThrottleRate = 0.1f;
+
 	void Update () {
 
 		float vert = Input.GetAxis ("Vertical");
@@ -50,23 +53,36 @@ public class InputManager : MonoBehaviour {
 		bool ctrlDown = Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl) || Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand);
 
 		float scroll = Input.GetAxis("Mouse ScrollWheel");
-		Camera.transform.Translate(0, 0, scroll * scrollSpeed, Space.World);
+		if(scroll >= 0.005f || scroll <= -0.005f)
+			Camera.transform.Translate(0, 0, scroll * scrollSpeed, Space.World);
 
-		if (Input.GetMouseButton (0)) {
+		if (Input.GetMouseButton (1)) {
 			rotationX += Input.GetAxis ("Mouse X") * sensX * Time.deltaTime;
 			rotationY += Input.GetAxis ("Mouse Y") * sensY * Time.deltaTime;
 			rotationY = Mathf.Clamp (rotationY, minY, maxY);
 			Camera.transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
 		}
 
+		float rotationXOffset = Input.GetAxis ("RightStickH") * sensX * Time.deltaTime;
+		float rotationYOffset = Input.GetAxis ("RightStickV") * sensY * Time.deltaTime;
+		if (rotationXOffset >= 0.05 || rotationYOffset >= 0.05 || rotationXOffset <= -0.05 || rotationYOffset <= -0.05) {
+			rotationX += rotationXOffset;
+			rotationY += rotationYOffset;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+			Camera.transform.localEulerAngles = new Vector3 (rotationY, rotationX, 0);
+		}
+
+		bool goFurther = Input.GetAxis ("LeftStickV") >= 1;
+		bool goCloser = Input.GetAxis ("LeftStickV") <= -1;
+
 		// Buttons for creating blocks
-		if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Joystick1Button0)) {
+		if (Input.GetKeyDown (KeyCode.Alpha1)|| Input.GetKeyDown (KeyCode.Joystick1Button3)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, Block.BlockType.Basic);
-		} else if (Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Joystick1Button1)) {
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, Block.BlockType.Ice);
-		} else if (Input.GetKeyDown (KeyCode.Alpha3) || Input.GetKeyDown (KeyCode.Joystick1Button2)) {
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, Block.BlockType.Heavy1);
-		} else if (Input.GetKeyDown (KeyCode.Alpha4) || Input.GetKeyDown (KeyCode.Joystick1Button3)) {
+		} else if (Input.GetKeyDown (KeyCode.Alpha4)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, Block.BlockType.Bomb1);
 		} else if (Input.GetKeyDown (KeyCode.Alpha5)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, Block.BlockType.Bomb2);
@@ -88,7 +104,7 @@ public class InputManager : MonoBehaviour {
 #endif
 		}
 		// Toggle block info display
-		else if (Input.GetKeyDown (KeyCode.I)) {
+			else if (Input.GetKeyDown (KeyCode.I) || Input.GetKeyDown (KeyCode.Joystick1Button6)) {
 			GetBlockManager ().ToggleBlockInfo ();
 		}
 		//Quit
@@ -104,25 +120,25 @@ public class InputManager : MonoBehaviour {
 		// Block type cycling
 		else if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Joystick1Button0)) {
 			GetBlockManager ().CreateBlockAtCursor (Cursor, GetBlockManager ().BlockCycleType);
-		} else if (Input.GetKeyDown (KeyCode.LeftBracket)) {
+		} else if (Input.GetKeyDown (KeyCode.LeftBracket) || Input.GetKeyDown (KeyCode.Joystick1Button4)) {
 			Block.BlockType type = GetBlockManager ().PrevBlockType ();
 			if(GetBlockManager().ActiveObject != null)
 				GetBlockManager ().CreateBlockAtCursor (Cursor, type);
-		} else if (Input.GetKeyDown (KeyCode.RightBracket)) {
+		} else if (Input.GetKeyDown (KeyCode.RightBracket)  || Input.GetKeyDown (KeyCode.Joystick1Button5)) {
 			Block.BlockType type = GetBlockManager ().NextBlockType ();
 			if(GetBlockManager().ActiveObject != null)
 				GetBlockManager ().CreateBlockAtCursor (Cursor, type);
 		}
 
 		// Destorying/modifying blocks
-		else if (Input.GetKeyDown (KeyCode.Delete) || Input.GetKeyDown (KeyCode.Joystick1Button7)) {
+		else if (Input.GetKeyDown (KeyCode.Delete) || Input.GetKeyDown (KeyCode.Joystick1Button1)) {
 			if (GetBlockManager ().ActiveObject != null)
 				//TODO use a method of BlockManager to do this
 				Destroy (GetBlockManager ().ActiveObject);
 		} 
 
 		// Buttons for setting items
-		else if (Input.GetKeyDown (KeyCode.Home)  || Input.GetKeyDown(KeyCode.Joystick1Button6)) {
+		else if (Input.GetKeyDown (KeyCode.Home)  || Input.GetKeyDown(KeyCode.Joystick1Button2)) {
 			if (GetBlockManager().ActiveObject != null) {
 				//TODO use a method of BlockManager to do this
 				Block block = (Block)GetBlockManager().ActiveObject.GetComponent (typeof(Block));
@@ -140,82 +156,94 @@ public class InputManager : MonoBehaviour {
 		// Keyboard & XBox 360 Input
 		// TODO update ActiveObject based on cursor position using colliders
 		else if (Input.GetKeyDown (KeyCode.UpArrow) || vert > 0) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			if (altDown) {
 				GetBlockManager ().ActiveFloor.transform.Rotate (90, 0, 0, Space.Self);
 				Cursor.transform.Rotate (90, 0, 0, Space.Self);
-				Input.ResetInputAxes ();
 			} else if (shiftDown) {
 				GameObject ao = GetBlockManager ().ActiveObject;
 				if (ao != null)
 					ao.transform.Translate (0, 1, 0);
 				Cursor.transform.position += new Vector3 (0, 1, 0);
-				Input.ResetInputAxes ();
 			} else if (ctrlDown) {
 				GetCamera().offset += new Vector3 (0, 1, 0);
-				Input.ResetInputAxes ();
 			} else {
 				Cursor.transform.position += new Vector3 (0, 1, 0);
-				Input.ResetInputAxes ();
 			}
 		} else if (Input.GetKeyDown (KeyCode.DownArrow) || vert < 0) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			if (altDown) {
 				GetBlockManager ().ActiveFloor.transform.Rotate (-90, 0, 0, Space.Self);
 				Cursor.transform.Rotate (-90, 0, 0, Space.Self);
-				Input.ResetInputAxes ();
 			} else if(shiftDown) {
 				GameObject ao = GetBlockManager ().ActiveObject;
 				if (ao != null)
 					ao.transform.Translate (0, -1, 0);
 				Cursor.transform.position += new Vector3 (0, -1, 0);
-				Input.ResetInputAxes ();
 			} else if (ctrlDown) {
 				GetCamera().offset += new Vector3 (0, -1, 0);
-				Input.ResetInputAxes ();
 			} else {
 				Cursor.transform.position += new Vector3 (0, -1, 0);
-				Input.ResetInputAxes ();
 			}
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow) || hori < 0) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			if (altDown) {
 				GetBlockManager ().ActiveFloor.transform.Rotate (0, 90, 0, Space.Self);
 				Cursor.transform.Rotate (0, 90, 0, Space.Self);
-				Input.ResetInputAxes ();
 			} else if(shiftDown) {
 				GameObject ao = GetBlockManager ().ActiveObject;
 				if (ao != null)
 					ao.transform.Translate (-1, 0, 0);
 				Cursor.transform.position += new Vector3 (-1, 0, 0);
-				Input.ResetInputAxes ();
 			} else if (ctrlDown) {
 				GetCamera().offset += new Vector3 (-1, 0, 0);
-				Input.ResetInputAxes ();
 			} else {
 				Cursor.transform.position += new Vector3 (-1, 0, 0);
-				Input.ResetInputAxes ();
 			}
 		} else if (Input.GetKeyDown (KeyCode.RightArrow) || hori > 0) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			if (altDown) {
 				GetBlockManager ().ActiveFloor.transform.Rotate (0, -90, 0, Space.Self);
 				Cursor.transform.Rotate (0, -90, 0, Space.Self);
-				Input.ResetInputAxes ();
 			} else if(shiftDown) {
 				GameObject ao = GetBlockManager ().ActiveObject;
 				if (ao != null)
 					ao.transform.Translate (1, 0, 0);
 				Cursor.transform.position += new Vector3 (1, 0, 0);
-				Input.ResetInputAxes ();
 			} else if (ctrlDown) {
 				GetCamera().offset += new Vector3 (1, 0, 0);
-				Input.ResetInputAxes ();
 			} else {
 				Cursor.transform.position += new Vector3 (1, 0, 0);
-				Input.ResetInputAxes ();
 			}
-		} else if (Input.GetKeyDown (KeyCode.Comma) || Input.GetKeyDown(KeyCode.Joystick1Button4)) {
-			Input.ResetInputAxes ();
+		} else if (Input.GetKeyDown (KeyCode.Comma) || goFurther) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			Cursor.transform.position += new Vector3 (0, 0, -1);
-		} else if (Input.GetKeyDown (KeyCode.Period) || Input.GetKeyDown(KeyCode.Joystick8Button5)) {
-			Input.ResetInputAxes ();
+		} else if (Input.GetKeyDown (KeyCode.Period) || goCloser) {
+			if (period < joystickThrottleRate) {
+				period += Time.deltaTime;
+				return;
+			}
+			period = 0.0f;
 			Cursor.transform.position += new Vector3 (0, 0, 1);
 		}
 	}
