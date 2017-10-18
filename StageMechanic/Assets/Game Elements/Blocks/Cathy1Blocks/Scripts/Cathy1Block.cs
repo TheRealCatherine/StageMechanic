8a8e00a2f3d6f7eb9a49a44b0d73685c5180c29c
Catherine <catherineirkalla@gmail.com>
2017-10-17 22:57:24 -0500

    Add copyright headers

--- StageMechanic/Assets/Game Elements/Blocks/Cathy1Blocks/Scripts/Cathy1Block.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Cathy1Blocks/Scripts/Cathy1Block.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*  
+ * Copyright (C) Catherine. All rights reserved.  
+ * Licensed under the BSD 3-Clause License.
+ * See LICENSE file in the project root for full license information.
+ * See CONTRIBUTORS file in the project root for full list of contributors.
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Serialization;
