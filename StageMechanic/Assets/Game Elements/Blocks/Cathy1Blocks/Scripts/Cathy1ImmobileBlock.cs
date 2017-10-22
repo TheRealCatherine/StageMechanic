9eb3e7080e33f2f724927432a0c70b201287fa87
Catherine <catherineirkalla@gmail.com>
2017-10-21 23:10:19 -0500

    Add constraint for not moving immobile blocks

---
+++ StageMechanic/Assets/Game Elements/Blocks/Cathy1Blocks/Scripts/Cathy1ImmobileBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cathy1ImmobileBlock : Cathy1Block {
+
+	public override Cathy1Block.BlockType Type {
+		get {
+			return Cathy1Block.BlockType.Immobile;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		WeightFactor = 0f;
+	}
+}
