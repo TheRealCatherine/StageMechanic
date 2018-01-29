61984d8bbc0d11aaac20fa9bdee8d07b76e623c4
Catherine <catherineirkalla@gmail.com>
2018-01-28 18:22:00 -0600

    improve Mystery Blocks

--- StageMechanic/Assets/Game Elements/Blocks/Cathy1Blocks/Scripts/Cathy1MysteryBlock.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Cathy1Blocks/Scripts/Cathy1MysteryBlock.cs
@@ -4,6 +4,7 @@
  * See LICENSE file in the project root for full license information.
  * See CONTRIBUTORS file in the project root for full list of contributors.
  */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,10 @@ using UnityEngine;
 public class Cathy1MysteryBlock : Cathy1Block
 {
     public sealed override BlockType Type { get; } = BlockType.Random;
+
+    public float Delay = DEFAULT_DELAY;
+    public const float DEFAULT_DELAY = 0.05f;
+
     public readonly string[] PossibleTypes = {
         "Basic",
         "Immobile",
@@ -26,28 +31,53 @@ public class Cathy1MysteryBlock : Cathy1Block
         "Monster",
         "Vortex"};
 
-    bool hasPlayer()
+    protected override void OnPlayerEnter(PlayerMovementEvent ev)
     {
-        Vector3 player = PlayerManager.Player1Location();
-        return (player == transform.position + Vector3.up && (PlayerManager.PlayerStateName() == "Idle" || PlayerManager.PlayerStateName() == "Walk" || PlayerManager.PlayerStateName() == "Center"));
+        base.OnPlayerEnter(ev);
+        StartCoroutine(HandlePlayer(ev));
     }
 
-    internal override void Update()
+    bool _started = false;
+    virtual internal IEnumerator HandlePlayer(PlayerMovementEvent ev)
     {
-        base.Update();
-        if (!BlockManager.PlayMode)
-            return;
-        if (!hasPlayer())
-            return;
-        StartCoroutine(HandleStep());
+        if (ev.Location != PlayerMovementEvent.EventLocation.Top || _started)
+            yield break;
+        string statename = ev.Player.StateNames[ev.Player.CurrentStateIndex];
+        if (statename == "Idle" || statename == "Walk" || statename == "Center")
+        {
+            _started = true;
+            GetComponent<AudioSource>()?.Play();
+            yield return new WaitForSeconds(Delay);
+            System.Random rnd = new System.Random();
+            int index = rnd.Next(PossibleTypes.Length);
+            BlockManager.CreateBlockAt(Position, "Cathy1 Internal", PossibleTypes[index]);
+        }
     }
 
-    private IEnumerator HandleStep()
+    public override Dictionary<string, KeyValuePair<Type, string>> DefaultProperties
     {
-        GetComponent<AudioSource>()?.Play();
-        yield return new WaitForSeconds(0.05f);
-        System.Random rnd = new System.Random();
-        int index = rnd.Next(PossibleTypes.Length);
-        BlockManager.CreateBlockAt(Position, "Cathy1 Internal", PossibleTypes[index]);
+        get
+        {
+            Dictionary<string, KeyValuePair<Type, string>> ret = base.DefaultProperties;
+            ret.Add("Trigger Time (seconds)", new KeyValuePair<Type, string>(typeof(float), DEFAULT_DELAY.ToString()));
+            return ret;
+        }
+    }
+
+    public override Dictionary<string, string> Properties
+    {
+        get
+        {
+            Dictionary<string, string> ret = base.Properties;
+            if (Delay != DEFAULT_DELAY)
+                ret.Add("Trigger Time (seconds)", Delay.ToString());
+            return ret;
+        }
+        set
+        {
+            base.Properties = value;
+            if (value.ContainsKey("Trigger Time (seconds)"))
+                Delay = float.Parse(value["Trigger Time (seconds)"]);
+        }
     }
 }
