78f3dbafcffd1dfd32f73e7007ed0cd29c2f4cb6
Catherine <catherineirkalla@gmail.com>
2018-02-08 12:23:29 -0600

    Add ESRB-style content warning flags to block themes

--- StageMechanic/Assets/Game Elements/Blocks/Interfaces/AbstractBlockTheme.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Interfaces/AbstractBlockTheme.cs
@@ -5,4 +5,20 @@ public abstract class AbstractBlockTheme : ScriptableObject
 {
 	public string Name;
 
+	[Header("Content Warnings")]
+	public bool Alcohol;
+	public bool Drugs;
+	public bool Sexuality;
+	public bool IntenseSexuality;
+	public bool Gambling;
+	public bool MatureLanguage;
+	public bool MatureHumor;
+
+	public bool Blood;
+	public bool Gore;
+	public bool ComicMiscief;
+	public bool CartoonViolence;
+	public bool FantasyViolence;
+	public bool IntenseViolence;
+	public bool SexualViolence;
 }
