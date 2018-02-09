ce81f5286dab529800794808d470cee5a3b230fa
Catherine <catherineirkalla@gmail.com>
2018-02-09 16:18:53 -0600

    Start on Bloxels support

---
+++ StageMechanic/Assets/Game Elements/Blocks/Bloxels1PlatformerBlocks/Scripts/BloxelsGroundBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloxelsGroundBlock : AbstractBlock
+{
+	public override void Awake()
+	{
+		base.Awake();
+		WeightFactor = 0;
+	}
+
+	public override string TypeName
+	{
+		get
+		{
+			return "Ground";
+		}
+
+		set
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
