8af741a149982c876b7101b53f3b64075abebb97
Catherine <catherineirkalla@gmail.com>
2018-02-10 15:10:28 -0600

    Gradient versions of bloxels blocks

--- StageMechanic/Assets/Game Elements/Blocks/Bloxels1PlatformerBlocks/Scripts/AbstractBloxelsBlock.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Bloxels1PlatformerBlocks/Scripts/AbstractBloxelsBlock.cs
@@ -4,10 +4,31 @@ using System.Collections;
 public abstract class AbstractBloxelsBlock : AbstractBlock
 {
 
+	MeshRenderer Placeholder;
+	MeshRenderer StaticMesh;
+
 	public override void Awake()
 	{
 		base.Awake();
 		GravityFactor = 0;
+	}
+
+	internal override void Update()
+	{
+		base.Update();
+		if (Placeholder != null && StaticMesh != null)
+		{
+			if (BlockManager.PlayMode)
+			{
+				StaticMesh.gameObject.SetActive(true);
+				Placeholder.gameObject.SetActive(false);
+			}
+			else
+			{
+				StaticMesh.gameObject.SetActive(false);
+				Placeholder.gameObject.SetActive(true);
+			}
+		}
 	}
 
 	public abstract override string TypeName { get; set; }
