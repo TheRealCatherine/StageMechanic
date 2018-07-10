8b05d82abd3ce1051a931adaddc86a6cdf1bf683
You're Perfect <youreperfectstudio@gmail.com>
2018-07-09 19:00:59 -0500

    Add an method to react to game mode change

--- StageMechanic/Assets/Game Elements/Blocks/Boxxyzzy/Scripts/AbstractBloxelsBlock.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Boxxyzzy/Scripts/AbstractBloxelsBlock.cs
@@ -25,19 +25,35 @@ public abstract class AbstractBloxelsBlock : AbstractBlock
 	internal override void Update()
 	{
 		base.Update();
+		if(BlockManager.PlayMode && _lastMode != GameManager.GameMode.Play)
+		{
+			OnGameModeChange(_lastMode, GameManager.GameMode.Play);
+		}
+		else if((!BlockManager.PlayMode) && _lastMode != GameManager.GameMode.StageEdit)
+		{
+			OnGameModeChange(_lastMode, GameManager.GameMode.StageEdit);
+		}
+	}
+
+	//TODO clean this up when refactoring BlockManager code into GameManager
+	//TODO move this to AbstractBlock
+	private GameManager.GameMode _lastMode = (BlockManager.PlayMode?GameManager.GameMode.Play:GameManager.GameMode.StageEdit);
+	internal virtual void OnGameModeChange( GameManager.GameMode oldMode, GameManager.GameMode newMode )
+	{
 		if (Placeholder != null && StaticMesh != null)
 		{
-			if (BlockManager.PlayMode)
-			{
-				StaticMeshInstance.gameObject.SetActive(true);
-				PlaceholderInstance.gameObject.SetActive(false);
-			}
-			else
+			if (oldMode == GameManager.GameMode.Play && newMode == GameManager.GameMode.StageEdit)
 			{
 				StaticMeshInstance.gameObject.SetActive(false);
 				PlaceholderInstance.gameObject.SetActive(true);
 			}
+			else if (oldMode == GameManager.GameMode.StageEdit && newMode == GameManager.GameMode.Play)
+			{
+				StaticMeshInstance.gameObject.SetActive(true);
+				PlaceholderInstance.gameObject.SetActive(false);
+			}
 		}
+		_lastMode = newMode;
 	}
 
 	public abstract override string TypeName { get; set; }
