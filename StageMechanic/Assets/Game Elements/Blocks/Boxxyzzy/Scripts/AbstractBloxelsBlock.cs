5ca311759c905a9025907811c92e396ddf829bd9
You're Perfect <youreperfectstudio@gmail.com>
2018-07-11 10:22:24 -0500

    Fix enemy turning into purple block on undo

--- StageMechanic/Assets/Game Elements/Blocks/Boxxyzzy/Scripts/AbstractBloxelsBlock.cs
+++ StageMechanic/Assets/Game Elements/Blocks/Boxxyzzy/Scripts/AbstractBloxelsBlock.cs
@@ -25,7 +25,23 @@ public abstract class AbstractBloxelsBlock : AbstractBlock
 	internal override void Update()
 	{
 		base.Update();
-		if(BlockManager.PlayMode && _lastMode != GameManager.GameMode.Play)
+
+		GameManager.GameMode currentMode = (BlockManager.PlayMode ? GameManager.GameMode.Play : GameManager.GameMode.StageEdit);
+		if (Placeholder != null && StaticMesh != null)
+		{
+			if (currentMode == GameManager.GameMode.StageEdit)
+			{
+				StaticMeshInstance.gameObject.SetActive(false);
+				PlaceholderInstance.gameObject.SetActive(true);
+			}
+			else if (currentMode == GameManager.GameMode.Play)
+			{
+				StaticMeshInstance.gameObject.SetActive(true);
+				PlaceholderInstance.gameObject.SetActive(false);
+			}
+		}
+
+		if (BlockManager.PlayMode && _lastMode != GameManager.GameMode.Play)
 		{
 			OnGameModeChange(_lastMode, GameManager.GameMode.Play);
 		}
@@ -37,22 +53,10 @@ public abstract class AbstractBloxelsBlock : AbstractBlock
 
 	//TODO clean this up when refactoring BlockManager code into GameManager
 	//TODO move this to AbstractBlock
+	//TODO this is not always getting called after an undo
 	private GameManager.GameMode _lastMode = (BlockManager.PlayMode?GameManager.GameMode.Play:GameManager.GameMode.StageEdit);
 	internal virtual void OnGameModeChange( GameManager.GameMode oldMode, GameManager.GameMode newMode )
 	{
-		if (Placeholder != null && StaticMesh != null)
-		{
-			if (oldMode == GameManager.GameMode.Play && newMode == GameManager.GameMode.StageEdit)
-			{
-				StaticMeshInstance.gameObject.SetActive(false);
-				PlaceholderInstance.gameObject.SetActive(true);
-			}
-			else if (oldMode == GameManager.GameMode.StageEdit && newMode == GameManager.GameMode.Play)
-			{
-				StaticMeshInstance.gameObject.SetActive(true);
-				PlaceholderInstance.gameObject.SetActive(false);
-			}
-		}
 		_lastMode = newMode;
 	}
 
