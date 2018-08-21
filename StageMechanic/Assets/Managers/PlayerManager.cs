98e90ed2b54110bd9fb06830a77f0b55d642b106
You're Perfect <youreperfectstudio@gmail.com>
2018-08-21 10:20:11 -0500

    Update copyright headers to say You're Perfect Studio

--- StageMechanic/Assets/Managers/PlayerManager.cs
+++ StageMechanic/Assets/Managers/PlayerManager.cs
@@ -1,5 +1,5 @@
 ﻿/*  
- * Copyright (C) Catherine. All rights reserved.  
+ * Copyright (C) 2018 You're Perfect Studio. All rights reserved.  
  * Licensed under the BSD 3-Clause License.
  * See LICENSE file in the project root for full license information.
  * See CONTRIBUTORS file in the project root for full list of contributors.
