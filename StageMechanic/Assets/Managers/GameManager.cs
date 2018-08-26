6c82f500a3c0f8adf4014743d8093084f2101dca
You're Perfect <youreperfectstudio@gmail.com>
2018-08-25 19:25:24 -0500

    Start improving game manager

--- StageMechanic/Assets/Managers/GameManager.cs
+++ StageMechanic/Assets/Managers/GameManager.cs
@@ -14,5 +14,30 @@ public class GameManager
 		TextureEdit,
 		Play
 	}
+
+	private static GameMode _gameMode = GameMode.Initialize;
+	private static GameMode _lastGameMode = GameMode.Initialize;
+	public static GameMode CurrentGameMode
+	{
+		get
+		{
+			if (_gameMode == GameMode.Initialize)
+				_gameMode = GameMode.StageEdit;
+			return _gameMode;
+		}
+		set
+		{
+			_lastGameMode = _gameMode;
+			_gameMode = value;
+		}
+	}
+
+	public static void TogglePlayMode()
+	{
+		if (_gameMode != GameMode.Play)
+			CurrentGameMode = GameMode.Play;
+		else
+			CurrentGameMode = _lastGameMode;
+	}
 }
 
