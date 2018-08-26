e9b55fc463c7b2db1478fe922f07c3c4ef5c2f8d
You're Perfect <youreperfectstudio@gmail.com>
2018-08-26 17:02:38 -0500

    Dark theme, new logo font, misc UI impriovements

---
+++ StageMechanic/Assets/UI/URLButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLButton : MonoBehaviour {
+
+	public string URL;
+
+	public void OnClicked()
+	{
+		Application.OpenURL(URL);
+	}
+}
