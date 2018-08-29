a7732abe7343cd89e2bbd8159c6c09d22828eefb
You're Perfect <youreperfectstudio@gmail.com>
2018-08-29 12:31:54 -0500

    use guitar instead of cow

---
+++ StageMechanic/Assets/Utility/SpinXAxis.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinXAxis : MonoBehaviour {
+
+		public float speed = 10f;
+
+		void Update()
+		{
+			transform.Rotate(Vector3.left, speed * Time.deltaTime);
+		}
+}
