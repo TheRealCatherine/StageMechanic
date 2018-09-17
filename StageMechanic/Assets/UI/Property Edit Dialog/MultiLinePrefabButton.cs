eae878e2f81e6063519681ccacb4e3957800d3b1
You're Perfect <youreperfectstudio@gmail.com>
2018-09-17 12:43:31 -0500

    multi line input window stuff

--- StageMechanic/Assets/UI/Property Edit Dialog/MultiLinePrefabButton.cs
+++ StageMechanic/Assets/UI/Property Edit Dialog/MultiLinePrefabButton.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultiLinePrefabButton : MonoBehaviour
 {
 	public GameObject TextEditorPrefab;
+	public InputField Display;
+	private GameObject Editor;
 
 	public void OnClick()
 	{
 		Canvas mainCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-		GameObject editor = Instantiate(TextEditorPrefab, mainCanvas.transform);
-		
+		Editor = Instantiate(TextEditorPrefab, mainCanvas.transform);
+		Editor.GetComponentInChildren<InputField>().text = Display.text;
+	}
+
+	private void Update()
+	{
+		if (Editor != null)
+		{
+			Display.text = Editor.GetComponentInChildren<InputField>().text;
+
+			if (!Editor.activeInHierarchy)
+			{
+				Destroy(Editor);
+				Editor = null;
+			}
+		}
 	}
 }
