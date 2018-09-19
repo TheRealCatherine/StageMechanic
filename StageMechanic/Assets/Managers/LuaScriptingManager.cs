f828c48c40951b5b8fcdac564e6a54d16dfef203
You're Perfect <youreperfectstudio@gmail.com>
2018-09-19 01:42:15 -0500

    Lua patternmatching

--- StageMechanic/Assets/Managers/LuaScriptingManager.cs
+++ StageMechanic/Assets/Managers/LuaScriptingManager.cs
@@ -9,16 +9,17 @@ public class LuaScriptingManager : MonoBehaviour
 	public static LuaScriptingManager Instance;
 	public const string Keywords = @"\b(and|break|do|else|elseif|end|false|for|function|if|in|local|nil|not|or|repeat|return|then|true|until|while)\b";
 	public const string Classes = @"\b(BlockManager|ItemManager|PlayerManager|AudioManager|VisualEffectsManager|SkyboxManager|Alert|coroutine|string|utf8|table|math|io|file|os|debug)\b";
-	public const string Functions = @"\b(assert|collectgarbage|dofile|error|_G|getmetatable|ipairs|load|loadfile|next|pairs|pcall|print|rawequal|rawget|rawlen|rawset|select|setmetatable|ronumber|tostring|type|_VERSION|xpcall" +
-		@"|coroutine.create|coroutine.isyieldable|coroutine.resume|coroutine.running|coroutine.status|coroutine.wrap|coroutine.yeld|"+
-		@"|package.congig|package.cpath|package.loaded|package.loadlib|package.path|package.preload|package.searchers|package.searchpath" +
-		@"|string.byte|string.char|string.dump|string.find|string.format|string.gmatch|string.gsub|string.len|string.lower|string.match|string.pack|string.packsize|string.rep|string.reverse|string.sub|string.unpack|string.upper" +
-		@"|utf8.char|utf8.charpattern|utf8.codes|utf8.codepoint|utf8.len|utf8.offset" +
-		@"|table.concat|table.insert|table.move|table.pack|table.remove|table.sort|table.unpack" +
-		@"|math.abs|math.acos|math.asin|math.atan|math.ceil|math.cos|math.deg|math.exp|math.floor|math.fmod|math.hugh|math.log|math.max|math.maxinteger|math.min|math.mininteger|math.modf|math.pi|math.rad|math.random|math.randomseed|math.sin|math.sqrt|math.tan|math.tointeger|math.type|math.ult" +
-		@"|io.cose|io.flush|io.input|io.lines|io.open|io.output|io.popen|io.read|io.tmpfile|io.type|io.write|file:close|file:flush|file:lines|file:read|file:seek|file:setvbuf|file:write" +
-		@"|os.clock" + //TODO finish this list
-		")\b";
+	public const string Functions = @"\b(assert|collectgarbage|dofile|error|_G|getmetatable|ipairs|load|loadfile|next|pairs|pcall|print|rawequal|rawget|rawlen|rawset|select|setmetatable|ronumber|tostring|type|_VERSION|xpcall"+
+		@"|coroutine\.create|coroutine\.isyieldable|coroutine\.resume|coroutine\.running|coroutine\.status|coroutine\.wrap|coroutine\.yield"+
+		@"|package\.congig|package\.cpath|package\.loaded|package\.loadlib|package\.path|package\.preload|package\.searchers|package\.searchpath" +
+		@"|string\.byte|string\.char|string\.dump|string\.find|string\.format|string\.gmatch|string\.gsub|string\.len|string\.lower|string\.match|string\.pack|string\.packsize|string\.rep|string\.reverse|string\.sub|string\.unpack|string\.upper" +
+		@"|utf8\.char|utf8\.charpattern|utf8\.codes|utf8\.codepoint|utf8\.len|utf8\.offset" +
+		@"|table\.concat|table\.insert|table\.move|table\.pack|table\.remove|table\.sort|table\.unpack" +
+		@"|math\.abs|math\.acos|math\.asin|math\.atan|math\.ceil|math\.cos|math\.deg|math\.exp|math\.floor|math\.fmod|math\.hugh|math\.log|math\.max|math\.maxinteger|math\.min|math\.mininteger|math\.modf|math\.pi|math\.rad|math\.random|math\.randomseed|math\.sin|math\.sqrt|math\.tan|math\.tointeger|math\.type|math\.ult" +
+		@"|io\.close|io\.flush|io\.input|io\.lines|io\.open|io\.output|io\.popen|io\.read|io\.tmpfile|io\.type|io\.write|file:close|file:flush|file:lines|file:read|file:seek|file:setvbuf|file:write" +
+		@"|os.clock|os.date|os.difftime|os.execute|os.exit|os.getenv|os.remove|os.rename|os.setlocale|os.time|os.tmpname" +
+		@"|debug.debug|debug.gethook|debug.getinfo|debug.getlocal|debug.getmetatable|debug.getregistry|debug.getupvalue|debug.getuservalue|debug.sethook|debug.setlocal|debug.setmetatable|debug.setupvalue|debug.setuservalue|debug.traceback|debug.upvalueid|debug.upvaluejoin" +
+		@")\b";
 	public const string Types = @"\b(nil|boolean|number|string|userdata|function|thread|table)\b";
 
 	private void Awake()
