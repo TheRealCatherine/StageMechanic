40fafa603e6b59b24d0afee617f66ddf793219bc
You're Perfect <youreperfectstudio@gmail.com>
2018-10-07 20:51:03 -0500

    allow bringing up the music dialog

---
+++ StageMechanic/Assets/CloseDialogButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDialogButton : MonoBehaviour
+{
+	public GameObject Dialog;
+
+	public void OnClicked()
+	{
+		Dialog.gameObject.SetActive(false);
+	}
+}
